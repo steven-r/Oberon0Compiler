edf360a53c2320f7ae3769c7e38bb4f4987900b6
Stephen Reindl <stephen@stephenreindl.net>
2016-10-18 19:15:27 +0200

    Additional Test to check for more than one statement

--- UnitTestProject1/SimpleStatements.cs
+++ UnitTestProject1/SimpleStatements.cs
@@ -29,11 +29,40 @@ END.
 ");
             Assert.AreEqual(1, m.Block.Statements.Count);
             Assert.IsAssignableFrom(typeof(AssignmentStatement), m.Block.Statements[0]);
-            AssignmentStatement ast = (AssignmentStatement) m.Block.Statements[0];
+            AssignmentStatement ast = (AssignmentStatement)m.Block.Statements[0];
             Assert.AreEqual("x", ast.Variable.Name);
             Assert.IsAssignableFrom(typeof(ConstantIntExpression), ast.Expression);
             ConstantIntExpression cie = (ConstantIntExpression)ast.Expression;
             Assert.AreEqual(1, cie.Value);
+        }
+        [Test]
+
+        public void TwoStatements()
+        {
+            var compiler = new CompilerParser();
+            Module m = compiler.Calculate(@"MODULE Test; 
+VAR
+  x: INTEGER;
+
+BEGIN 
+    x := 1;
+    x := 2;
+END.
+");
+            Assert.AreEqual(2, m.Block.Statements.Count);
+            Assert.IsAssignableFrom(typeof(AssignmentStatement), m.Block.Statements[0]);
+            AssignmentStatement ast = (AssignmentStatement)m.Block.Statements[0];
+            Assert.AreEqual("x", ast.Variable.Name);
+            Assert.IsAssignableFrom(typeof(ConstantIntExpression), ast.Expression);
+            ConstantIntExpression cie = (ConstantIntExpression)ast.Expression;
+            Assert.AreEqual(1, cie.Value);
+
+            Assert.IsAssignableFrom(typeof(AssignmentStatement), m.Block.Statements[1]);
+            ast = (AssignmentStatement)m.Block.Statements[1];
+            Assert.AreEqual("x", ast.Variable.Name);
+            Assert.IsAssignableFrom(typeof(ConstantIntExpression), ast.Expression);
+            cie = (ConstantIntExpression)ast.Expression;
+            Assert.AreEqual(2, cie.Value);
         }
     }
 }
