6a10ff93af584050988f69b5e069839474639e3e
Stephen Reindl <stephen@stephenreindl.net>
2016-10-19 20:56:26 +0200

    Fix sonarqube markup that sub has lhs and rhs identical

--- oberon0/Expressions/Arithmetic/SubExpression.cs
+++ oberon0/Expressions/Arithmetic/SubExpression.cs
@@ -24,14 +24,14 @@ namespace Oberon0.Compiler.Expressions.Arithmetic
                 return null;
             }
             // 1. Easy as int
-            var lhi = (ConstantExpression)LeftHandSide;
-            var rhi = (ConstantExpression)RightHandSide;
-            if (rhi.BaseType == lhi.BaseType && lhi.BaseType == BaseType.IntType)
+            var leftHandSide = (ConstantExpression)LeftHandSide;
+            var rightHandSide = (ConstantExpression)RightHandSide;
+            if (rightHandSide.BaseType == leftHandSide.BaseType && leftHandSide.BaseType == BaseType.IntType)
             {
-                return new ConstantIntExpression(lhi.ToInt32() - rhi.ToInt32());
+                return new ConstantIntExpression(leftHandSide.ToInt32() - rightHandSide.ToInt32());
             }
             // at least one of them is double
-            return new ConstantDoubleExpression(lhi.ToDouble() - lhi.ToDouble());
+            return new ConstantDoubleExpression(leftHandSide.ToDouble() - rightHandSide.ToDouble());
         }
     }
 }
