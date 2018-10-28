e2b546b41806c79e2312262316f46d97c7a14169
Stephen Reindl <stephen.reindl@outlook.de>
2018-10-29 00:25:04 +0100

    fix SQ code smell (complexity)

--- oberon0/Definitions/Module.StandardFunctions.cs
+++ oberon0/Definitions/Module.StandardFunctions.cs
@@ -1,33 +1,117 @@
-﻿using System;
-using System.Reflection;
-using Oberon0.Attributes;
-using Oberon0.Compiler.Expressions;
-using Oberon0.Compiler.Types;
+﻿#region copyright
+// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Module.StandardFunctions.cs" company="Stephen Reindl">
+// Copyright (c) Stephen Reindl. All rights reserved.
+// Licensed under the MIT license. See LICENSE.md file in the project root for full license information.
+// </copyright>
+// <summary>
+//     Part of oberon0 - Oberon0Compiler/Module.StandardFunctions.cs
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+#endregion
 
 namespace Oberon0.Compiler.Definitions
 {
-    using Oberon0.Compiler.Expressions.Constant;
+    using System;
+    using System.Reflection;
 
+    using Oberon0.Attributes;
+    using Oberon0.Compiler.Expressions.Constant;
+    using Oberon0.Compiler.Types;
+
+    /// <summary>
+    /// The module.
+    /// </summary>
     public partial class Module
     {
+        private void AddParameters(Oberon0ExportAttribute attr, int i, ProcedureParameter[] procParameters)
+        {
+            var isVar = false;
+            var paramType = attr.Parameters[i];
+            if (paramType.StartsWith("VAR ", StringComparison.InvariantCultureIgnoreCase))
+            {
+                isVar = true;
+                paramType = paramType.Substring(4).Trim(); // skip "VAR "
+            }
+            else if (paramType.StartsWith("&"))
+            {
+                isVar = true;
+                paramType = paramType.Substring(1).Trim();
+            }
+
+            var paramName = "attr" + i;
+            TypeDefinition td = this.Block.LookupType(paramType);
+            if (td == null)
+            {
+                throw new InvalidOperationException($"type {attr.Parameters[i]} not found");
+            }
+
+            procParameters[i] = new ProcedureParameter(paramName, this.Block, td, isVar);
+        }
+
+        private void DeclareStandardConsts()
+        {
+            this.Block.Declarations.Add(
+                new ConstDeclaration("TRUE", this.Block.LookupType("BOOLEAN"), new ConstantBoolExpression(true)));
+            this.Block.Declarations.Add(
+                new ConstDeclaration("FALSE", this.Block.LookupType("BOOLEAN"), new ConstantBoolExpression(false)));
+        }
+
         private void DeclareStandardFunctions()
-        {  
+        {
             // hardwired
-            Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("WriteInt", this, new ProcedureParameter("any", Block, Block.LookupType("INTEGER"), false)));
-            Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("WriteString", this, new ProcedureParameter("any", Block, Block.LookupType("STRING"), false)));
-            Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("WriteReal", this, new ProcedureParameter("any", Block, Block.LookupType("REAL"), false)));
-            Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("WriteLn", this));
-            Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("ReadInt", this, new ProcedureParameter("any", Block, Block.LookupType("INTEGER"), true)));
-            Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("ReadReal", this, new ProcedureParameter("any", Block, Block.LookupType("REAL"), true)));
+            this.Block.Procedures.Add(
+                FunctionDeclaration.AddHardwiredFunction(
+                    "WriteInt",
+                    this,
+                    new ProcedureParameter("any", this.Block, this.Block.LookupType("INTEGER"), false)));
+            this.Block.Procedures.Add(
+                FunctionDeclaration.AddHardwiredFunction(
+                    "WriteString",
+                    this,
+                    new ProcedureParameter("any", this.Block, this.Block.LookupType("STRING"), false)));
+            this.Block.Procedures.Add(
+                FunctionDeclaration.AddHardwiredFunction(
+                    "WriteReal",
+                    this,
+                    new ProcedureParameter("any", this.Block, this.Block.LookupType("REAL"), false)));
+            this.Block.Procedures.Add(FunctionDeclaration.AddHardwiredFunction("WriteLn", this));
+            this.Block.Procedures.Add(
+                FunctionDeclaration.AddHardwiredFunction(
+                    "ReadInt",
+                    this,
+                    new ProcedureParameter("any", this.Block, this.Block.LookupType("INTEGER"), true)));
+            this.Block.Procedures.Add(
+                FunctionDeclaration.AddHardwiredFunction(
+                    "ReadReal",
+                    this,
+                    new ProcedureParameter("any", this.Block, this.Block.LookupType("REAL"), true)));
 
             var asm = Assembly.Load("Oberon0.System");
             foreach (Type type in asm.GetExportedTypes())
             {
                 if (type.GetCustomAttribute<Oberon0LibraryAttribute>() != null)
                 {
-                    LoadLibraryMembers(type);
+                    this.LoadLibraryMembers(type);
                 }
             }
+        }
+
+        private void DeclareStandardTypes()
+        {
+            SimpleTypeDefinition.IntType = new SimpleTypeDefinition(BaseTypes.IntType, "INTEGER", true);
+            SimpleTypeDefinition.BoolType = new SimpleTypeDefinition(BaseTypes.BoolType, "BOOLEAN", true);
+            SimpleTypeDefinition.RealType = new SimpleTypeDefinition(BaseTypes.DecimalType, "REAL", true);
+            SimpleTypeDefinition.StringType = new SimpleTypeDefinition(BaseTypes.StringType, "STRING", true);
+            SimpleTypeDefinition.VoidType = new SimpleTypeDefinition(
+                BaseTypes.VoidType,
+                TypeDefinition.VoidTypeName,
+                true);
+            this.Block.Types.Add(SimpleTypeDefinition.IntType);
+            this.Block.Types.Add(SimpleTypeDefinition.BoolType);
+            this.Block.Types.Add(SimpleTypeDefinition.RealType);
+            this.Block.Types.Add(SimpleTypeDefinition.StringType);
+            this.Block.Types.Add(SimpleTypeDefinition.VoidType);
         }
 
         /// <summary>
@@ -37,7 +121,7 @@ namespace Oberon0.Compiler.Definitions
         private void LoadLibraryMembers(Type type)
         {
             AssemblyName name = new AssemblyName(type.Assembly.FullName);
-            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public|BindingFlags.Static))
+            foreach (MethodInfo method in type.GetMethods(BindingFlags.Public | BindingFlags.Static))
             {
                 var attr = method.GetCustomAttribute<Oberon0ExportAttribute>();
                 if (attr == null) continue; // this method is not officially exported
@@ -45,63 +129,27 @@ namespace Oberon0.Compiler.Definitions
                 {
                     throw new InvalidOperationException("method not static");
                 }
-                var rt = Block.LookupType(attr.ReturnType);
+
+                var rt = this.Block.LookupType(attr.ReturnType);
                 if (rt == null)
                 {
                     throw new InvalidOperationException($"return type {attr.ReturnType} not found");
                 }
+
                 ProcedureParameter[] procParameters = new ProcedureParameter[attr.Parameters.Length];
                 for (int i = 0; i < attr.Parameters.Length; i++)
                 {
-                    bool isVar = false;
-                    string paramType = attr.Parameters[i];
-                    if (paramType.StartsWith("VAR ", StringComparison.InvariantCultureIgnoreCase))
-                    {
-                        isVar = true;
-                        paramType = paramType.Substring(4).Trim(); // skip "VAR "
-                    } else if (paramType.StartsWith("&"))
-                    {
-                        isVar = true;
-                        paramType = paramType.Substring(1).Trim();
-                    }
-                    string paramName = "attr" + i;
-                    TypeDefinition td = Block.LookupType(paramType);
-                    if (td == null)
-                    {
-                        throw new InvalidOperationException($"type {attr.Parameters[i]} not found");
-                    }
-                    procParameters[i] = new ProcedureParameter(paramName, Block, td, isVar);
+                    this.AddParameters(attr, i, procParameters);
                 }
-                var fd = new FunctionDeclaration(attr.Name, Block, rt, procParameters)
-                {
-                    Prototype = $"[{name.Name}]Oberon0.{type.Name}::{method.Name}",
-                    IsExternal = true,
-                };
-                Block.Procedures.Add(fd);
+
+                var fd = new FunctionDeclaration(attr.Name, this.Block, rt, procParameters)
+                             {
+                                 Prototype = $"[{name.Name}]Oberon0.{type.Name}::{method.Name}", IsExternal = true,
+                             };
+                this.Block.Procedures.Add(fd);
             }
+
             this.ExternalReferences.Add(type.Assembly);
-
-        }
-
-        private void DeclareStandardTypes()
-        {
-            SimpleTypeDefinition.IntType = new SimpleTypeDefinition(BaseTypes.IntType, "INTEGER", true);
-            SimpleTypeDefinition.BoolType = new SimpleTypeDefinition(BaseTypes.BoolType, "BOOLEAN", true);
-            SimpleTypeDefinition.RealType = new SimpleTypeDefinition(BaseTypes.DecimalType, "REAL", true);
-            SimpleTypeDefinition.StringType = new SimpleTypeDefinition(BaseTypes.StringType, "STRING", true);
-            SimpleTypeDefinition.VoidType = new SimpleTypeDefinition(BaseTypes.VoidType, TypeDefinition.VoidTypeName,
-                true);
-            Block.Types.Add(SimpleTypeDefinition.IntType);
-            Block.Types.Add(SimpleTypeDefinition.BoolType);
-            Block.Types.Add(SimpleTypeDefinition.RealType);
-            Block.Types.Add(SimpleTypeDefinition.StringType);
-            Block.Types.Add(SimpleTypeDefinition.VoidType);
-        }
-
-        private void DeclareStandardConsts()
-        {
-            Block.Declarations.Add(new ConstDeclaration("TRUE", Block.LookupType("BOOLEAN"), new ConstantBoolExpression(true)));
-            Block.Declarations.Add(new ConstDeclaration("FALSE", Block.LookupType("BOOLEAN"), new ConstantBoolExpression(false)));
         }
     }
 }
