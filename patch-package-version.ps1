83854b4d26d894db36eb379d03cd43335736aa81
Stephen Reindl <stephen.reindl@outlook.de>
2020-07-02 11:13:48 +0200

    Fix encoding on output

--- patch-package-version.ps1
+++ patch-package-version.ps1
@@ -1,3 +1,3 @@
 (Get-Content 'package.json') | 
 	Foreach-Object {$_ -replace '"version": "[0-9.]+"',"""version"": ""$env:GitVersion_SemVer""" } | 
-	Out-File "package.json"
+	Out-File -Encoding Default "package.json"
